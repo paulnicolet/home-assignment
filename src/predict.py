2b84b8b77fac498209fb12eef9e932c1cadbcef6
Paul Nicolet <nicoletpaul@icloud.com>
2020-10-04 10:53:03 +0200

    Rename load_model function

--- src/predict.py
+++ src/predict.py
@@ -14,14 +14,14 @@ def has_tomatoes(img_path, checkpoint_path):
     Returns:
         bool: True if the image contains traces of tomato, False otherwise.
     """
-    model = _load_model(checkpoint_path)
+    model = load_model(checkpoint_path)
     tensor = TomatoesDataset.prepare_single_image(img_path)
     output = model(tensor.unsqueeze(0))
     label_index = torch.max(output, dim=1)[1][0]
     return bool(label_index)
 
 
-def _load_model(checkpoint_path):
+def load_model(checkpoint_path):
     model = new_model()
     model.load_state_dict(torch.load(checkpoint_path))
     model.eval()
